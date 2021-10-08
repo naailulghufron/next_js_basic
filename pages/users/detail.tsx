5746f6987adec983389095e530f6d4ace4e49270
naailulghufron <naailul@yahoo.com>
2021-10-09 04:48:25 +0700

    basic routing

---
+++ pages/users/detail.tsx
@@ -0,0 +1,7 @@
+export default function detail() {
+    return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,7 @@
+export default function index() {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque vel maxime beatae recusandae impedit dolorum nesciunt autem totam ut, veniam molestias, minus obcaecati aut voluptatum ducimus cupiditate magni alias porro assumenda rerum velit dolores nostrum quasi voluptates. Mollitia ipsam illo doloremque eligendi eaque accusamus blanditiis tempora repellat impedit dolore, sequi est eveniet laborum officiis voluptatum totam at nemo. Cumque nam excepturi harum! Sed, libero architecto magnam ab quidem reiciendis! Officiis ex iusto maiores exercitationem ipsum nisi dicta iure perspiciatis dolorum, in natus molestias quidem sunt, consequuntur vero, porro error voluptates eligendi iste. Necessitatibus iure inventore quasi repudiandae quis et sapiente unde voluptatum ducimus voluptate libero, nobis laboriosam ad cum possimus recusandae doloribus corporis sit iusto. Quae quibusdam cumque error ipsa. Quas doloribus recusandae quia aliquam quod animi in temporibus accusantium sit minus, voluptates tempora dolorem laborum officiis odit ad exercitationem commodi facere reiciendis nisi, necessitatibus, optio adipisci? Necessitatibus, quae ullam.</p>
+        </div>
+    )
+}
