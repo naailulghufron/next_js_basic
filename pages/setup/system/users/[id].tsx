72be09ac05a2740a5c4d62c10ca8845dec26aea6
naailulghufron <naailul@yahoo.com>
2021-10-10 22:14:56 +0700

    bug fix interface user

--- pages/setup/system/users/[id].tsx
+++ pages/setup/system/users/[id].tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router"
 import Layout from "../../../../components/Layout/Index"
 
 interface userDetailProps{
-    user: object
+    user: any
 }
 
 export default function UserDetail(props: userDetailProps) {
@@ -13,8 +13,8 @@ export default function UserDetail(props: userDetailProps) {
         <Layout>
             <div>
                 <h1>Hello world</h1>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
+                <p>{user['name']}</p>
+                <p>{user['email']}</p>
             </div>
         </Layout>
     )
@@ -25,7 +25,7 @@ export async function getStaticPaths(){
     const dataUsers = await res.json()
 
     // console.log(dataUsers)
-    const paths = dataUsers.map((user) => ({
+    const paths = dataUsers.map((user: { id: any }) => ({
             params:{
                 id : `${user.id}`,
             }
@@ -37,13 +37,13 @@ export async function getStaticPaths(){
     }
 }
 
-export async function getStaticProps(context){
+export async function getStaticProps(context: { params: { id: any } }){
     const {id} = context.params
 
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     const user = await res.json()
 
-    console.log(user)
+    // console.log(user)
     
     return {
         props: {
